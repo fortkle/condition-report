6cfac9355ee387ae403d1df19361d49263b8326c
fortkle <fortkle@gmail.com>
2020-04-05 22:54:43 +0900

    hello world

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-function hello(name: string): string {
-    return `Hello, ${name}`
-}
+import { App } from '@slack/bolt'
+import { config } from 'dotenv'
 
-console.log(hello("world"))
+config()
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+})
+
+;(async () => {
+  app.message('hello', async ({ say }) => {
+    say(`hello!`)
+  })
+
+  const port = process.env.PORT || 3000
+  await app.start(port)
+
+  console.log('⚡️condition-report is running!')
+})()
